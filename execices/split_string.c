e70ceae05b5332e5d1ba60c04adc2e7558a6196a
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2023-04-17 16:20:14 +0200

    fix free memory in the function

--- execices/split_string.c
+++ execices/split_string.c
@@ -9,7 +9,7 @@ char **split_string(char *str)
 	char *token = NULL;
 	char **array_of_words;
 	int nb_words = 0;
-	int i = 0;
+	int i = 0, j = 0;
 
 	for (; str_cp[i] != '\0'; i++)
 	{
@@ -20,13 +20,17 @@ char **split_string(char *str)
 	if (nb_words == 0)
 	{
 		printf("Add string\n");
-		exit (-1);
+		free(str_cp);
+		exit(-1);
 	}
 	nb_words += 1;
 
 	array_of_words = malloc(sizeof(char *) * nb_words);
 	if (array_of_words == NULL)
+	{
+		free(str_cp);
 		return (0);
+	}
 
 	token = strtok(str_cp, " ");
 
@@ -35,23 +39,32 @@ char **split_string(char *str)
 		array_of_words[i] = malloc(sizeof(char) * strlen(token) + 1);
 		if (array_of_words[i] == NULL)
 		{
+			for (; j < i; j++) // Libérer la mémoire allouée pour les chaînes précédentes
+				free(array_of_words[j]);
 			free(array_of_words);
 			array_of_words = NULL;
+			free(str_cp);
 			return (0);
-		}	
-		array_of_words[i] = token;
+		}
+		strcpy(array_of_words[i], token);
 		printf("%s\n", array_of_words[i]);
 		token = strtok(NULL, " ");
 	}
-	free(token);
+	free(str_cp);
 	return (array_of_words);
 }
 
 int main(void)
 {
 	char *str = "Vlad Hugo Axel Benjmain";
-	
-	split_string(str);
+	char **words = NULL;
+	int i = 0;
+
+	words = split_string(str);
+
+	for (; words[i] != NULL; i++)
+		free(words[i]);
+	free(words);
 
 	return (0);
 }
