93ee738553044ae92bd915826279fdbf7e3f03c5
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2023-04-17 16:01:38 +0200

    write wplit string function

--- execices/split_string.c
+++ execices/split_string.c
@@ -1,5 +1,57 @@
+#include <stddef.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+char **split_string(char *str)
+{
+	char *str_cp = strdup(str);
+	char *token = NULL;
+	char **array_of_words;
+	int nb_words = 0;
+	int i = 0;
+
+	for (; str_cp[i] != '\0'; i++)
+	{
+		if (str_cp[i] == ' ' && str_cp[i + 1] != ' ')
+			nb_words++;
+	}
+
+	if (nb_words == 0)
+	{
+		printf("Add string\n");
+		exit (-1);
+	}
+	nb_words += 1;
+
+	array_of_words = malloc(sizeof(char *) * nb_words);
+	if (array_of_words == NULL)
+		return (0);
+
+	token = strtok(str_cp, " ");
+
+	for (i = 0; i < nb_words; i++)
+	{
+		array_of_words[i] = malloc(sizeof(char) * strlen(token) + 1);
+		if (array_of_words[i] == NULL)
+		{
+			free(array_of_words);
+			array_of_words = NULL;
+			return (0);
+		}	
+		array_of_words[i] = token;
+		printf("%s\n", array_of_words[i]);
+		token = strtok(NULL, " ");
+	}
+	free(token);
+	return (array_of_words);
+}
+
 int main(void)
 {
+	char *str = "Vlad Hugo Axel Benjmain";
 	
+	split_string(str);
+
 	return (0);
 }
