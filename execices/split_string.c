97703f47251857dfff51ee4e2849ce942d384173
hug0-cstrs <hugo.casteras@outlook.fr>
2023-04-19 00:37:55 +0200

    add indent

--- execices/split_string.c
+++ execices/split_string.c
@@ -1,61 +1,61 @@
 #include <stddef.h>
 #include <stdio.h>
 #include <string.h>
-#include <stdlib.h>   
+#include <stdlib.h>
 
 /*Function to find delimiter*/
 unsigned int is_delim(char c, char *delim)
 {
-    while(*delim != '\0')
-    {
-        if(c == *delim)
-            return 1;
-        delim++;
-    }
-    return 0;
+	while (*delim != '\0')
+	{
+		if (c == *delim)
+			return 1;
+		delim++;
+	}
+	return 0;
 }
 
 /*Own implementation of strtok*/
 char *_strtok(char *src, char *delim)
 {
-    /*start of the next search*/
-    static char *backup_string;
-    char *token = NULL;
+	/*start of the next search*/
+	static char *backup_string;
+	char *token = NULL;
 
-    if(!src)
-        src = backup_string;
+	if (!src)
+		src = backup_string;
 
-    if(!src)
-        return NULL;
+	if (!src)
+		return NULL;
 
-    /* ignore delims at the beginning of the string */
-    while(1)
-    {
-        if(is_delim(*src, delim))
-        {
-            src++;
-            continue;
-        }
-        if(*src == '\0')
-            return NULL; 
-        break;
-    }
-    token = src;
-    while(1)
-    {
-        if(*src == '\0')
-        {
-            backup_string = src;
-            return token;
-        }
-        if(is_delim(*src, delim))
-        {
-            *src = '\0';
-            backup_string = src + 1;
-            return token;
-        }
-        src++;
-    }
+	/* ignore delims at the beginning of the string */
+	while (1)
+	{
+		if (is_delim(*src, delim))
+		{
+			src++;
+			continue;
+		}
+		if (*src == '\0')
+			return NULL;
+		break;
+	}
+	token = src;
+	while (1)
+	{
+		if (*src == '\0')
+		{
+			backup_string = src;
+			return token;
+		}
+		if (is_delim(*src, delim))
+		{
+			*src = '\0';
+			backup_string = src + 1;
+			return token;
+		}
+		src++;
+	}
 }
 
 /*Function to split a string and store in an array of strings*/
