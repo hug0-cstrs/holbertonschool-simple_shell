6a4064d9bae2c005fc515e1925f5b4dafdb8660a
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2023-04-18 10:44:57 +0200

    Write a function that splits a string and returns an array of each word of the string with our own strtok implementation

--- execices/split_string.c
+++ execices/split_string.c
@@ -1,8 +1,64 @@
 #include <stddef.h>
 #include <stdio.h>
 #include <string.h>
-#include <stdlib.h>
+#include <stdlib.h>   
 
+/*Function to find delimiter*/
+unsigned int is_delim(char c, char *delim)
+{
+    while(*delim != '\0')
+    {
+        if(c == *delim)
+            return 1;
+        delim++;
+    }
+    return 0;
+}
+
+/*Own implementation of strtok*/
+char *_strtok(char *src, char *delim)
+{
+    /*start of the next search*/
+    static char *backup_string;
+    char *token = NULL;
+
+    if(!src)
+        src = backup_string;
+
+    if(!src)
+        return NULL;
+
+    /* ignore delims at the beginning of the string */
+    while(1)
+    {
+        if(is_delim(*src, delim))
+        {
+            src++;
+            continue;
+        }
+        if(*src == '\0')
+            return NULL; 
+        break;
+    }
+    token = src;
+    while(1)
+    {
+        if(*src == '\0')
+        {
+            backup_string = src;
+            return token;
+        }
+        if(is_delim(*src, delim))
+        {
+            *src = '\0';
+            backup_string = src + 1;
+            return token;
+        }
+        src++;
+    }
+}
+
+/*Function to split a string and store in an array of strings*/
 char **split_string(char *str)
 {
 	char *str_cp = strdup(str);
@@ -32,14 +88,14 @@ char **split_string(char *str)
 		return (0);
 	}
 
-	token = strtok(str_cp, " ");
+	token = _strtok(str_cp, " ");
 
 	for (i = 0; i < nb_words; i++)
 	{
 		array_of_words[i] = malloc(sizeof(char) * strlen(token) + 1);
 		if (array_of_words[i] == NULL)
 		{
-			for (; j < i; j++) // Libérer la mémoire allouée pour les chaînes précédentes
+			for (; j < i; j++) /* Libérer la mémoire allouée pour les chaînes précédentes */
 				free(array_of_words[j]);
 			free(array_of_words);
 			array_of_words = NULL;
@@ -48,15 +104,16 @@ char **split_string(char *str)
 		}
 		strcpy(array_of_words[i], token);
 		printf("%s\n", array_of_words[i]);
-		token = strtok(NULL, " ");
+		token = _strtok(NULL, " ");
 	}
 	free(str_cp);
 	return (array_of_words);
 }
 
+/*Main for test*/
 int main(void)
 {
-	char *str = "Vlad Hugo Axel Benjmain";
+	char *str = "Vlad Hugo Axel Benjmain Hey";
 	char **words = NULL;
 	int i = 0;
 
