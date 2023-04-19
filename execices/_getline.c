d8528c150e882aaf1b70f4a0319918bf4e53c1a2
hug0-cstrs <hugo.casteras@outlook.fr>
2023-04-19 10:26:45 +0200

    changes comments to handle \n

--- execices/_getline.c
+++ execices/_getline.c
@@ -44,6 +44,13 @@ ssize_t _getline(char **line, size_t *linesize, FILE *stream)
 		/*A virer le \n et le remplacer par \0 --> string terminée par \0\0*/
 		if ((*line)[strlen(*line) - 1] == '\n')
 		return (strlen(*line));
-	}
+
+		/* code adapter pour super_simple shell :
+		if ((*line)[strlen(*line) - 1] == '\n')
+		{
+			(*line)[strlen(*line) - 1] = '\0'; /* On supprime le caractère '\n' */
+			return (strlen(*line));
+		}
+
 	return (-1);
 }
