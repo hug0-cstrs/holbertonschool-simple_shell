c0d903b4f988f7a4e66d7e61a1db28fc8f6c9f70
hug0-cstrs <hugo.casteras@outlook.fr>
2023-04-19 10:27:40 +0200

    decomment main for test

--- execices/split_string.c
+++ execices/split_string.c
@@ -110,7 +110,7 @@ char **split_string(char *str)
 	return (array_of_words);
 }
 
-/*
+
 int main(void)
 {
 	char *str = "Vlad Hugo Axel Benjmain Hey";
@@ -125,4 +125,4 @@ int main(void)
 
 	return (0);
 }
-*/
+
