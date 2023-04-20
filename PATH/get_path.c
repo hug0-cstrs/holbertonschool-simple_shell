59a9701183d8d48e7ff0a0abfabb198e9ecef670
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2023-04-20 06:56:29 -0700

    write a function that get the PATH environment variable value in one single string

--- PATH/get_path.c
+++ PATH/get_path.c
@@ -1,25 +1,34 @@
 #include "simple_shell.h"
 
-int get_path(char *envp[])
+/**
+ * get_path - get the PATH environment variable value in one single string
+ * @envp: an array of pointers with environment variables of the system
+ *
+ * Return: a pointer to a copy of the PATH environment variable value
+ */
+char *get_path(char *envp[])
 {
 	int i = 0;
-	char *token_name;
-	char *token_value;
-	char *temp;
-	char *path = "PATH";
+	char *token_name = NULL;
+	char *token_value = NULL;
+	char *temp = NULL;
+	char *path_name = "PATH";
+	char *token_value_cp = NULL;
 
 	while (envp[i] != NULL)
 	{
 		temp = strdup(envp[i]);
 		token_name = _strtok(temp, "=");
-		if ((_strcmp(token_name, path)) == 0)
+		if ((_strcmp(token_name, path_name)) == 0)
 		{
-			printf("%s", token_name);
 			token_value = _strtok(NULL, "=");
-			printf("---->%s\n", token_value);
+			break;
 		}
 		free(temp);
 		i++;
 	}
-	return 0;
+	token_value_cp = strdup(token_value);
+	free(temp);
+
+	return (token_value_cp);
 }
