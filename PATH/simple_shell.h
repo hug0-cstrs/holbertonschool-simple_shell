37073d5337718f2bcc21021d07b208898f41e8b9
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2023-04-20 06:57:20 -0700

    add function prototypes to the header file

--- PATH/simple_shell.h
+++ PATH/simple_shell.h
@@ -14,6 +14,7 @@ char *_strtok(char *src, char *delim);
 ssize_t _getline(char **line, size_t *linesize, FILE *stream);
 int execute_command(char *command);
 int _strcmp(char *s1, char *s2);
-int get_path(char *envp[]);
+char *get_path(char *envp[]);
+char **get_path_values(char *token_value);
 
 #endif
