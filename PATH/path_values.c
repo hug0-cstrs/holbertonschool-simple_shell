91f4145d0b32c75c95a6b3f12ed2c6b2236dcea7
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2023-04-21 02:19:27 -0700

    create new source file with get_path and  get_path_value functions  remove corresponding source files

--- PATH/path_values.c
+++ PATH/path_values.c
@@ -1,6 +1,39 @@
 #include "simple_shell.h"
 
 /**
+ * get_path - get the PATH environment variable value in one single string
+ * @envp: an array of pointers with environment variables of the system
+ *
+ * Return: a pointer to a copy of the PATH environment variable value
+ */
+char *get_path(char *envp[])
+{
+	int i = 0;
+	char *token_name = NULL;
+	char *token_value = NULL;
+	char *temp = NULL;
+	char *path_name = "PATH";
+	char *token_value_cp = NULL;
+
+	while (envp[i] != NULL)
+	{
+		temp = strdup(envp[i]);
+		token_name = _strtok(temp, "=");
+		if ((_strcmp(token_name, path_name)) == 0)
+		{
+			token_value = _strtok(NULL, "=");
+			break;
+		}
+		free(temp);
+		i++;
+	}
+	token_value_cp = strdup(token_value);
+	free(temp);
+
+	return (token_value_cp);
+}
+
+/**
  * get_path_values - stores PATH value in an arrays table
  * @token_value: string containing PATH variable value in one string
  *
@@ -42,3 +75,4 @@ char **get_path_values(char *token_value)
 	free(temp);
 	return (path_values);
 }
+
