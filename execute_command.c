a45d376ce6aa205446362356d9dcf42bc87b221d
hug0-cstrs <hugo.casteras@outlook.fr>
2023-04-24 13:04:18 +0200

    add comments for create_args_array, check_command and free_args_array

--- execute_command.c
+++ execute_command.c
@@ -1,5 +1,18 @@
 #include "simple_shell.h"
-
+/**
+ * create_args_array - function that creates an array of arguments
+ * from a command string
+ * @command: string containing the command to execute
+ *
+ * Description: This function takes in a command string and tokenizes it using
+ * the delimiter " " to create an array of arguments. It allocates memory for
+ * the arguments array and each argument using malloc(). The last element of
+ * the array is set to NULL. If the command string is empty or allocation fails
+ * it returns NULL.
+ *
+ * Return: Returns a pointer to the array of arguments on success,
+ * NULL on failure
+*/
 char **create_args_array(char *command)
 {
 	int i = 0;
@@ -8,10 +21,7 @@ char **create_args_array(char *command)
 
 	arg = _strtok(command, " ");
 	if (arg == NULL)
-	{
-		// free(arg);
 		return (NULL);
-	}
 
 	args_array = malloc(sizeof(char *) * 256);
 	if (args_array == NULL)
@@ -27,6 +37,26 @@ char **create_args_array(char *command)
 	return (args_array);
 }
 
+/**
+ * check_command - function that checks if a command exists and is executable
+ * @args_array: array of arguments for the command
+ * @path_values: An array of strings containing the path values
+ * @flag: pointer to an integer to determine the execution mode of the command
+ *
+ * Description: This function takes in an array of arguments for a command and
+ * an array of path values. It checks if command is a valid path by checking
+ * if the first character is "/". If it is not a valid path, it searches the
+ * path values for command by concatenating each path value with the command
+ * name. It uses stat() system call to check if the command exists and is
+ * executable.
+ * If the command is not found, it prints "Unknown command" and returns NULL.
+ * If the command is found as a valid paththe flag is set to 1. If the command
+ * is found by searching the path values, the flag is set to 2. It returns the
+ * full path of the command on success, NULL on failure.
+ *
+ * Return: Returns a pointer to a string containing the full path of the
+ * command on success, NULL on failure.
+*/
 char *check_command(char **args_array, char **path_values, int *flag)
 {
 	char *strcat = NULL;
@@ -69,10 +99,22 @@ char *check_command(char **args_array, char **path_values, int *flag)
 
 	return (strcat);
 }
-
+/**
+ * free_args_array - function that free memory allocated for an arguments array
+ * @args_array: array of arguments to free
+ *
+ * Description: This function takes in an array of arguments and frees
+ * the memory
+ * allocated for each argument and the array itself using free().
+ * If the arguments
+ * array is empty, it does nothing.
+ *
+ * Return: void
+*/
 void free_args_array(char **args_array)
 {
 	int i = 0;
+
 	while (args_array[i])
 	{
 		free(args_array[i]);
@@ -127,7 +169,7 @@ int execute_command(char **path_values, char *command)
 			{
 				free_args_array(args_array);
 				free(strcat);
-			}	
+			}
 			else if (flag == 1)
 				free_args_array(args_array);
 			return (1);
