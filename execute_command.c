1bd845da691f4203c4a483f83889eb0e2b889b3e
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2023-04-25 02:02:58 -0700

    try fix valgrind leaks

--- execute_command.c
+++ execute_command.c
@@ -179,12 +179,12 @@ int execute_command(char **path_values, char *command, char **argv)
 			return (1);
 		}
 	}
-	if (args_array != NULL)
-		free_args_array(args_array);
 	if (path_values == NULL)
 	{
 		fprintf(stderr, "%s: 1: %s: not found\n", argv[0], args_array[0]);
 		exit(127);
 	}
+	if (args_array != NULL)
+		free_args_array(args_array);
 	return (1);
 }
