175a16316c10660375a56a7ffd68f0693045acc9
hug0-cstrs <hugo.casteras@outlook.fr>
2023-04-27 12:36:23 +0200

    created new all functions for fork command

--- execute_command.c
+++ execute_command.c
@@ -12,7 +12,7 @@
  *
  * Return: Returns a pointer to the array of arguments on success,
  * NULL on failure
-*/
+ */
 char **create_args_array(char *command)
 {
 	int i = 0;
@@ -56,7 +56,7 @@ char **create_args_array(char *command)
  *
  * Return: Returns a pointer to a string containing the full path of the
  * command on success, NULL on failure.
-*/
+ */
 char *check_command(char **args_array, char **path_values, int *flag)
 {
 	char *strcat = NULL;
@@ -105,6 +105,48 @@ char *check_command(char **args_array, char **path_values, int *flag)
 	return (strcat);
 }
 
+int _fork(int fl, char **args_a, char *strcat, char *cmd,
+char **path_v, char *path)
+{
+	pid_t pid;
+	int execve_res = 0, status, exit_status;
+
+	pid = fork();
+	if (pid == -1)
+		exit(1);
+	else if (pid == 0)
+	{
+		if (fl == 1)
+			execve_res = execve(args_a[0], args_a, environ);
+		else if (fl == 2)
+			execve_res = execve(strcat, args_a, environ);
+		if (execve_res == -1)
+			return (-1);
+	}
+	else
+	{
+		wait(&status);
+		if (WIFEXITED(status)) /* 1 if child executed normally */
+		{
+			exit_status = WEXITSTATUS(status);
+			if (exit_status != 0) /* if non null, means error at execution */
+			{
+				if (fl == 2)
+					_free(cmd, args_a, path_v, path, strcat);
+				else if (fl == 1)
+					_free(cmd, args_a, path_v, path, NULL);
+				exit(2);
+			}
+		}
+		if (fl == 2)
+			_free(NULL, args_a, NULL, NULL, strcat);
+		else if (fl == 1)
+			free_args_array(args_a);
+		return (1);
+	}
+	return (0);
+}
+
 /**
  * execute_command - function that executes a command
  * @command: string containing the command to execute
@@ -124,72 +166,38 @@ char *check_command(char **args_array, char **path_values, int *flag)
  */
 int execute_command(char **path_values, char *command, char **argv, char *path)
 {
-	int execve_res = 0, status, fl = 0, exit_status;
-	pid_t pid;
+	int fl = 0, fork_res = 0;
 	char **args_array = create_args_array(command), *_exit = "exit";
 	char *strcat = check_command(args_array, path_values, &fl);
 	int path1 = search_path1(environ);
 
 	if (fl)
+		fork_res = _fork(fl, args_array, strcat, command, path_values, path);
+	if (fork_res == 0)
 	{
-		pid = fork();
-		if (pid == -1)
+		if (args_array != NULL && (_strcmp(args_array[0], _exit) == 0))
 		{
-			perror("Error: child process failed");
-			exit(1);
+			_free(command, args_array, path_values, path, NULL);
+			exit(0);
 		}
-		else if (pid == 0)
+		if (path1 == -1)
 		{
-			if (fl == 1)
-				execve_res = execve(args_array[0], args_array, environ);
-			else if (fl == 2)
-				execve_res = execve(strcat, args_array, environ);
-			if (execve_res == -1)
-				return (-1);
-		}
-		else
-		{
-			wait(&status);
-			if (WIFEXITED(status)) /* 1 if child executed normally */
+			if (args_array != NULL)
 			{
-				exit_status = WEXITSTATUS(status);
-				if (exit_status != 0) /* if non null, means error at execution */
-				{
-					if (fl == 2)
-						_free(command, args_array, path_values, path, strcat);
-					else if (fl == 1)
-						_free(command, args_array, path_values, path, NULL);
-					exit(2);
-				}
-			}
-			if (fl == 2)
-				_free(NULL, args_array, NULL, NULL, strcat);
-			else if (fl == 1)
+				fprintf(stderr, "%s: 1: %s: not found\n", argv[0], args_array[0]);
 				free_args_array(args_array);
-			return (1);
+				return (0);
+			}
 		}
-	}
-	if (args_array != NULL && (_strcmp(args_array[0], _exit) == 0))
-	{
-		_free(command, args_array, path_values, path, NULL);
-		exit(0);
-	}
-	if (path1 == -1)
-	{
-		if (args_array != NULL)
+		if ((path_values != NULL && *path_values[0] == '\0')
+		|| fl == 0 || path1 == 1)
 		{
-			fprintf(stderr, "%s: 1: %s: not found\n", argv[0], args_array[0]);
-			free_args_array(args_array);
-			return (0);
-		}
-	}
-	if ((path_values != NULL && *path_values[0] == '\0') || fl == 0 || path1 == 1)
-	{
-		if (args_array != NULL)
-		{
-			fprintf(stderr, "%s: 1: %s: not found\n", argv[0], args_array[0]);
-			free_args_array(args_array);
-			return (0);
+			if (args_array != NULL)
+			{
+				fprintf(stderr, "%s: 1: %s: not found\n", argv[0], args_array[0]);
+				free_args_array(args_array);
+				return (0);
+			}
 		}
 	}
 	return (2);
