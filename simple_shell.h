2ef863029812e734aa2d9f4dfc0306909892bf9e
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2023-04-28 11:27:38 +0200

    removed struct description from header

--- simple_shell.h
+++ simple_shell.h
@@ -13,30 +13,6 @@
 
 extern char **environ;
 
-/**
- * struct stat - Structure containing information about a file
- * @st_dev: Device ID of the device containing the file
- * @st_ino: Inode number of the file
- * @st_mode: File type and mode (permissions)
- * @st_nlink: Number of hard links to the file
- * @st_uid: User ID of the file owner
- * @st_gid: Group ID of the file owner
- * @st_rdev: Device ID (if file is a special file)
- * @st_size: Total file size, in bytes
- * @st_blksize: Block size for filesystem I/O
- * @st_blocks: Number of 512B blocks allocated for the file
- * @st_atime: Time of last access
- * @st_mtime: Time of last modification
- * @st_ctime: Time of last status change
- *
- * Description: The stat structure is used to store information about a file.
- * It is utilized by the stat(), fstat() and lstat() system calls to retrieve
- * information about the file such as its size, permissions, and creation time.
- * The structure contains fields such as the file's inode number, owner and
- * group IDs, protection mode, size, and timestamps.
- */
-struct stat;
-
 char *_strtok(char *src, char *delim);
 ssize_t _getline(char **line, size_t *linesize, FILE *stream);
 int _strcmp(char *s1, char *s2);
@@ -46,10 +22,13 @@ char *_strcatcp(char *dest, char *src);
 char *_strcpy(char *dest, char *src);
 char **create_args_array(char *command);
 char *check_command(char **args_array, char **path_values, int *flag);
-int execute_command(char **path_values, char *command, char **argv, char *path);
+int execute_command(char **path_values, char *command,
+char **argv, char *path);
 void free_args_array(char **args_array);
 int search_path1(char **environ);
-void _free(char *cmd, char **args_a, char **path_v, char *path, char *strcat);
-int _fork(int fl, char **args_a, char *strcat, char *cmd, char **path_v, char *path);
+void _free(char *cmd, char **args_a, char **path_v,
+char *path, char *strcat);
+int _fork(int fl, char **args_a, char *strcat, char *cmd,
+char **path_v, char *path);
 
 #endif
